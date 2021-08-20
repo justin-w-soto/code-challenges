0342a7555b38550e8d330314a0222eda70fb97ff
Justin W Soto <soto@pdx.edu>
2021-08-20 11:55:24 -0700

    all tests passing

--- src/more-object-keys-entries.js
+++ src/more-object-keys-entries.js
@@ -1,20 +1,32 @@
 export function getHouses(obj) {
-    return
-}
+    return Object.values(obj).map(obj => obj.house);
+};
 
 export function updateNumbers(obj) {
-    return
-}
+    return Object.entries(obj).map(obj => obj[0] + ": " + obj[1]);
+};
 
 export function totalCharacters(arr) {
-    return
-}
-
+    let count = 0;
+    Object.values(arr).forEach(arr => {
+        count++;
+        count += arr.children.length;
+        if (arr.spouse) count++;
+        
+    });
+    return count;
+};
 
 export function hasChildrenEntries(arr, character) {
-    return
-} 
+    let statement = false;
+    const matching = arr.find(obj => obj.name === character);
+
+    if (matching.children[0] !== undefined) {
+        statement = true;
+    }
+    return statement;
+}; 
 
 export function sortByChildren(arr){
-    return
-} 
+    return arr.sort((a, b) => a.children.length - b.children.length);
+};
--- src/more-object-keys-entries.test.js
+++ src/more-object-keys-entries.test.js
@@ -1,6 +1,6 @@
-import { getHouses } from './more-object-keys-entries.js';
-test('it gets the names of the houses', () => {
-  const characters = [
+import { getHouses, updateNumbers, totalCharacters, hasChildrenEntries, sortByChildren } from './more-object-keys-entries.js';
+
+const characters = [
     {
       name: 'Eddard',
       spouse: 'Catelyn',
@@ -43,7 +43,47 @@ test('it gets the names of the houses', () => {
       children: [],
       house: 'Snow'
     }
-  ]; 
+  ];
+
+
+test('it gets the names of the houses', () => {
   const output = getHouses(characters); 
-  expect(output).toEqual(['Greyjoy', 'Snow', 'Arryn', 'Tyrell', 'Lannister', 'Targaryen', 'Stark']); 
+  expect(output).toEqual(['Stark', 'Arryn', 'Lannister', 'Targaryen', 'Tyrell', 'Greyjoy', 'Snow']); 
 });
+
+
+test('it transforms value pairs to strings', () => {
+  const characters = 
+    { 'Grace Hopper': '222-303-5938', 'Ada Lovelace': '222-349-9842', 'Alan Turing': '222-853-5933' }
+  ; //arrange
+  const output = updateNumbers(characters); 
+  expect(output).toEqual(['Grace Hopper: 222-303-5938','Ada Lovelace: 222-349-9842','Alan Turing: 222-853-5933']); 
+});
+
+
+test('it counts the number of characters', () => {
+    const output = totalCharacters(characters); 
+    expect(output).toEqual(26); 
+});
+
+test('it returns true or false', () => {
+  
+    const output = hasChildrenEntries(characters, 'Eddard');
+    expect(output).toEqual(true);
+    const output2 = hasChildrenEntries(characters, 'Euron');
+    expect(output2).toEqual(false);
+    
+  });
+
+  test('it sorts by least to most children', () => {
+    const output = sortByChildren(characters); 
+    expect(output).toEqual([ 
+      {name: 'Euron', spouse: null, children: [], house: 'Greyjoy'},
+      { name: 'Jon S.', spouse: null, children: [], house: 'Snow'}, 
+      {name: 'Jon A.', spouse: 'Lysa', children: ['Robin'], house: 'Arryn'},
+      {name: 'Mace', spouse: 'Alerie', children: ['Margaery', 'Loras'], house: 'Tyrell'}, 
+      { name: 'Cersei',spouse: 'Robert', children: ['Joffrey', 'Myrcella', 'Tommen'], house: 'Lannister'},
+      {name: 'Daenarys', spouse: 'Khal Drogo', children: ['Drogon', 'Rhaegal', 'Viserion'], house: 'Targaryen'},
+      {name: 'Eddard', spouse: 'Catelyn', children: ['Robb', 'Sansa', 'Arya', 'Bran', 'Rickon'], house: 'Stark'}]); 
+  });
+  
